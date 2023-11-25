0e1dac577b383959e69cf787bc7d47146471a331
112-mariem-mansour <mansourmariemti12@gmail.com>
2023-11-25 11:10:04 +0100

    myaccount

--- Screens/HomeScreen/MyAccount.js
+++ Screens/HomeScreen/MyAccount.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacityBase, TouchableHighlightBase } from 'react-native'
 import React from 'react'
 import firebase from '../../Config'
 const database=firebase.database();
@@ -6,6 +6,15 @@ export default function MyAccount() {
   return (
     <View>
       <Text>MyAccount</Text>
+      <TouchableHighlight onPress={()=>{
+        const ref_profils=database.ref("profils");
+        const key =ref_profils.push().key;
+        const ref_un_profil=ref_profils.child("profil"+key) ;
+        ref_un_profil.set({
+            nom:nom,
+            prenom:prenom,
+            numero:numero,
+        })  }}></TouchableHighlight>
     </View>
   )
 }
